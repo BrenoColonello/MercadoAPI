81dea70d5ac5152503e6583bb107b1668287281c
BrenoColonello <brenosony1@gmail.com>
2026-02-18 22:19:12 -0300

    Handle transient SQL startup failures

--- MercadoApi/Program.cs
+++ MercadoApi/Program.cs
@@ -8,8 +8,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MercadoDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? "Server=localhost;Database=MercadoDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True"));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? "Server=localhost;Database=MercadoDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True",
+        sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
 builder.Services.AddCors(options =>
 {
@@ -25,8 +27,17 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
     var db = scope.ServiceProvider.GetRequiredService<MercadoDbContext>();
-    db.Database.EnsureCreated();
+    try
+    {
+        db.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        // Avoid crashing the app if the database is temporarily unavailable.
+        logger.LogError(ex, "Database init failed");
+    }
 }
 
 app.UseCors();
