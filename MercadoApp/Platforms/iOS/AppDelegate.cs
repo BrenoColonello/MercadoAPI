b72de8c04f05ec6e3df27024a9073940db6355b2
BrenoColonello <brenosony1@gmail.com>
2026-02-23 20:10:05 -0300

    Remove unused MAUI app and local artifacts

--- MercadoApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MercadoApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
